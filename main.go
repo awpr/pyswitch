81b7a1ccea5c515879fab83be52589a6cd5c6fab
Andrew Pritchard <awpritchard@gmail.com>
2012-12-09 02:25:55 -0600

    handle $PYTHON being a filename; not that this allows stupid things like 'PYTHON=/usr/bin/echo python'

--- main.go
+++ main.go
@@ -13,23 +13,34 @@ var versions = map[string]string {
 }
 
 func main() {
+	var path string
 	python := os.Getenv("PYTHON")
-	path, _ := versions[python]
 
+	// if $PYTHON is a path to an executable, use it; if not, try to expand the
+	// shortcuts listed above
+	if _, err := os.Stat(python); err == nil {
+		path = python
+	} else {
+		path, _ = versions[python]
+	}
+
+	// make sure we have something to exec
 	if _, err := os.Stat(path); err != nil {
 		log.Fatalf("unknown python version '%s'\n", python)
 	}
 
-	args := os.Args
-	args[0] = path
+	// give python our arguments but the path to the python executable
+	os.Args[0] = path
 
-	p, err := os.StartProcess(path, args, &os.ProcAttr{
+	// start python connected to our fds 0, 1, and 2
+	p, err := os.StartProcess(path, os.Args, &os.ProcAttr{
 		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
 	})
 	if err != nil {
 		log.Fatalf("failed to start python: %v\n", err)
 	}
 
+	// wait for it to execute, then clean up
 	s, err := p.Wait()
 	if err != nil {
 		log.Fatalf("failed to wait for python: %v\n", err)
